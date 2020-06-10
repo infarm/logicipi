4451df149e7b0352e39f15f0da4153bb15421fa8
Christian Barra <me@christianbarra.com>
2020-06-10 12:42:42 +0200

    Add new tests

--- tests/test_function.py
+++ tests/test_function.py
@@ -1,4 +1,4 @@
-from logicipi.functions import LogSeverity, stackdriver_logger_patcher
+from logicipi.function import Logger, LogSeverity, stackdriver_logger_patcher
 
 
 def test_stackdriver_call_with_event_and_kwargs(mocker):
@@ -27,3 +27,20 @@ def test_stackdriver_call_with_dict(mocker):
 
     [call] = logger_mock.log_struct.call_args_list
     assert call[0] == ({'event': 'my_event', 'data': 1},)
+
+
+def test_Logger():
+    function_name = 'my-function'
+    region = 'my-region'
+    instance = Logger(function_name=function_name, region=region)
+
+    assert instance.function_name == function_name
+    assert instance.region == region
+
+
+def test_Logger_singleton():
+    instance_1 = Logger(function_name='my-function', region="my-region")
+    instance_2 = Logger(function_name='my-function', region="my-region")
+
+    assert instance_1 is instance_2
+    assert instance_1.get_logger() is instance_2.get_logger()
---
+++ tests/utils.py
@@ -0,0 +1,36 @@
+from contextlib import contextmanager
+
+from structlog import DropEvent, configure, get_config
+
+
+class LogCapture:
+    """
+    Class for capturing log messages in its entries list.
+    but you can use this class if you want to capture logs with other patterns.
+    """
+
+    def __init__(self):
+        self.entries = []
+
+    def __call__(self, _, method_name, event_dict):
+        # event_dict is a tuple with with multiple elements
+        # (({log dict},),{dict with extra info})
+        self.entries.append(event_dict[0][0])
+        raise DropEvent
+
+
+@contextmanager
+def capture_logs():
+    """
+    Context manager that appends all logging statements to its yielded list
+    while it is active.
+
+    It appends a LogCapture to the existing processors.
+    """
+    cap = LogCapture()
+    old_processors = get_config()["processors"]
+    try:
+        configure(processors=[*old_processors, cap])
+        yield cap.entries
+    finally:
+        configure(processors=old_processors)
