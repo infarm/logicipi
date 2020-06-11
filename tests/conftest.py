1dc7d6ee66a9ca4e66128f65584d7c51d239a335
Christian Barra <barrachri@gmail.com>
2020-06-11 12:08:25 +0200

    Improve logging and use env variables to set function_name and region (#3)
    * Improve logging and use env variables to set function_name and region

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from structlog import configure
 from structlog._config import _Configuration
 
+from logicipi.function import Logger
+
 
 @pytest.fixture
 def reset_structlog_conf():
@@ -9,3 +11,11 @@ def reset_structlog_conf():
     Reset structlog configuration to default processors.
     """
     yield configure(processors=_Configuration.default_processors)
+
+
+@pytest.fixture(scope="function", autouse=True)
+def reset_singleton():
+    """Reset the Logger singleton"""
+    if hasattr(Logger, "_instance"):
+        del Logger._instance
+    yield
